4c499411bc130a44ced18e10f75c8046bc2606de
Shepherd Kendall <skendall@gmail.com>
2013-09-04 16:13:18 -0400

    annoying classloader related stuff

--- src/main/java/org/kndl/util/classloader/KndlClassLoader.java
+++ src/main/java/org/kndl/util/classloader/KndlClassLoader.java
@@ -2,11 +2,15 @@ package org.kndl.util.classloader;
 
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 /**
  * Loads a class from any selection of remote jars.
@@ -19,8 +23,11 @@ public class KndlClassLoader extends ClassLoader {
 
     private List<URLClassLoader> jarLocations;
 
+    private List<Class> loadedClasses;
+    
     public KndlClassLoader() {
         this.jarLocations = new LinkedList<URLClassLoader>();
+        this.loadedClasses = new ArrayList<Class>();
     }
 
     public void addLocation(URL url) {
@@ -31,35 +38,71 @@ public class KndlClassLoader extends ClassLoader {
     public Class load(String className) {
         Class c = null;
         for(URLClassLoader loader : jarLocations) {
-            System.out.println("Loading " + loader.getURLs().toString());
+        	for(URL url : loader.getURLs())
+        		System.out.println("Loading " + url);
             try {
                 c = Class.forName(className,true,loader);
             } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+                logger.error(e);
             }
         }
         return c;
-
     }
+    
+	public List<Class> loadAll() {
+		loadedClasses.clear();
+		
+		for (URLClassLoader loader : jarLocations) {
+			for (URL url : loader.getURLs()) {
+				logger.info("Loading: " + url);
+				try {
+					JarInputStream stream = new JarInputStream(url.openStream());
+					JarEntry entry = stream.getNextJarEntry();
 
-    public Class get(String className) {
-        try {
-            return Class.forName(className,true,this);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+					while (entry != null) {
+						String name = entry.getName();
+						if (name.endsWith(".class")) {
+							int i = name.lastIndexOf("/");
+
+							try {
+								Class c = Class.forName(name.substring(0, name.length() - 6).replace("/", "."), 
+										false,
+										loader);
+								loadedClasses.add(c);
+								logger.info("Loaded " + c.getName());
+							} catch (ClassNotFoundException e) {
+								// TODO Auto-generated catch block
+								logger.error(e.getMessage());
+							}
+						}
+						entry = stream.getNextJarEntry();
+					}
+
+					stream.close();
+				} catch (IOException ioe) {
+					logger.error(ioe);
+				}
+			}
+		}
+
+		return loadedClasses;
+	}
+	
+	public List<Class> getClasses() {
+		return loadedClasses;
+	}
 
     public static void main(String args[]) {
         KndlClassLoader ncl = new KndlClassLoader();
         try {
             ncl.addLocation(new URL("http://pants.spacerobots.org/test.jar"));
+            ncl.addLocation(new URL("http://repo1.maven.org/maven2/io/undertow/undertow-core/1.0.0.Beta12/undertow-core-1.0.0.Beta12.jar"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        ncl.load("Test");
-        Class c = ncl.get("Test");
-        System.out.println(c.getSimpleName());
+        List<Class> classes = ncl.loadAll();
+        for(Class c : classes) {
+        	System.out.println("Class: " + c.getName());
+        }
     }
 }
