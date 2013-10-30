19c1800d7adaa106918ebc20123a72560d275839
Shepherd Kendall <skendall@contentanalyst.com>
2013-10-30 08:09:36 -0400

    added string shortening

---
+++ src/main/java/org/kndl/util/IDUtil.java
@@ -0,0 +1,81 @@
+package org.kndl.util;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Simple object to generate numbers based on certain
+ * sets of criteria.
+ * 
+ * @author skendall
+ *
+ */
+
+public class IDUtil {
+	
+	/**
+	 * Used for the rolling integer index
+	 */
+	
+	private static final AtomicInteger				IDX_ID = new AtomicInteger();
+
+    /**
+     * Character map used in generating base-64 hash
+     */
+
+    private static final char[] CHAR_MAP= new char[] {
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '0','1','2','3','4','5','6','7','8','9','-','_'};
+
+    /**
+	 * Generates a unique ID based on the following algorithm.
+	 * 
+	 * The top 56 bits are generated from the current time
+	 * in milliseconds (epoch).  The last 8 bits are generated 
+	 * by a rolling integer index.  
+	 * 
+	 * This ensures a unique ID until a time when Java may have
+	 * long since expired.
+	 * 
+	 * @return
+	 */
+	
+	public final long nextUnique() {
+		int idx = 0;
+		if(IDX_ID.get() >= 256)
+			IDX_ID.set(0);
+		else
+			idx = IDX_ID.getAndIncrement();
+		long id = System.currentTimeMillis();
+		id = (id << 8) | idx;		
+		return id;
+    }
+
+    /**
+     * Generates a unique base-62 hash string based on a string
+     * passed to the method.
+     *
+     * @param str
+     * @return
+     */
+
+    public final String shorten(String str) {
+        if(str == null || str.isEmpty())
+            return "";
+
+        StringBuilder hash = new StringBuilder();
+
+        int dividend = str.hashCode();
+        int remainder = 0;
+
+        while(dividend != 0) {
+            remainder = dividend & 63;
+            dividend = dividend >>> 6;
+            hash.append(CHAR_MAP[remainder]);
+        }
+
+        return hash.toString();
+    }
+
+
+}
